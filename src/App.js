17390a5aea6ba0da8793341a16fcd004c96d898b
crypner <36641608+crypner@users.noreply.github.com>
2022-11-22 16:34:36 +0100

    modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	new file:   public/solana-logo.svg 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div className="container container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="mynavbar">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Contact</a>
+            </li>
+          </ul>
+          <div className="d-flex">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/">Login/Register</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <p>This is a React.js Template for Solana DApp Projects</p> 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
